7042a7e86c6c35e9ed165aa15b7e267ac1de5d1f
Christopher Anderson <cja@google.com>
2018-06-01 12:19:36 -0700

    [dev][platform-bus] Move MMIO/IRQ allocations to proxy devhosts
    This change integrates the new Resource Dispatcher model into the
    ARM platform.
    
    - Change bus proxy drivers so that on creation they request resource
      handles for their mmios and interruptss from the platform bus.
    - Use resources to create vmos and interrupts in the proxy devhost
    
    Change-Id: I8ea30c87a94952d0314f62a02ec6343bba37e03e

--- system/dev/bus/platform/platform-proxy.h
+++ system/dev/bus/platform/platform-proxy.h
@@ -106,10 +106,12 @@ typedef struct {
         pdev_i2c_txn_ctx_t i2c_txn;
         size_t i2c_max_transfer;
         struct {
-            zx_off_t offset;
-            size_t length;
             zx_paddr_t paddr;
+            size_t length;
         } mmio;
+        struct {
+            uint32_t irq;
+        } irq;
         pdev_device_info_t info;
         uint32_t scpi_sensor_value;
         uint16_t scpi_dvfs_idx;
